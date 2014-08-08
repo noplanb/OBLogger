c936f2ed249008ad06898828757738ef9e8b73fe
Sani Elfishawy <sani@sbcglobal.net>
2014-08-07 19:39:14 -0700

    Release 0.0.1.

---
+++ Example/Pods/Pods-OBLogger-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// OBLogger
+#define COCOAPODS_POD_AVAILABLE_OBLogger
+#define COCOAPODS_VERSION_MAJOR_OBLogger 0
+#define COCOAPODS_VERSION_MINOR_OBLogger 1
+#define COCOAPODS_VERSION_PATCH_OBLogger 0
+
